4265b9f2ed10523b625210aec2407625c5d4fa9c
klein994 <klein994@hotmail.com>
2020-01-04 19:48:04 -0300

    first commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,3 +12,22 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+//button
+var mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
